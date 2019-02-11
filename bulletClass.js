4e88d68ca9d70a8bc2b84b7b6536a94253b5a04b
Makotek <makoteq@gmail.com>
2019-02-11 10:12:04 +0100

    enemy_class

---
+++ bulletClass.js
@@ -0,0 +1,19 @@
+  class Bullet {
+        constructor(x, y, speed) {
+          this.x = x;
+          this.y = y;
+          this.speed = speed;
+          this.visible = false;
+          this.img = new Image();
+          this.img.src = "img/Fireball.png";
+        }
+      
+        speedUp() {
+          this.y -= this.speed;
+        }
+        setX(x) {
+          this.x=x;
+        }
+      }
+      export default  Bullet;
+                                                                                                        
---
+++ enemyClass.js
@@ -0,0 +1,18 @@
+ class enemyClass {
+        constructor(x, y, speed) {
+          this.x = x;
+          this.y = y;
+          this.speed = speed;
+          this.visible = false;
+          this.img = new Image();
+          this.img.src = "img/Asteroid.png";
+        }
+      
+        speedUp() {
+          this.y += this.speed;
+        }
+        setX(x) {
+          this.x=x;
+        }
+      }
+  export  enemyClass;
