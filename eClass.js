bba8d173dc50350d9a7be4460ea1d0f3817d5d73
Makotek <makoteq@gmail.com>
2019-02-24 12:59:55 +0100

    :cat:

--- eClass.js
+++ eClass.js
@@ -4,8 +4,7 @@
           this.visible = false;
           this.img = new Image();
           this.setPosition(0,50);
-          this.setSpeed(2);
-          this.setType(1) ;                
+          this.setSpeed(2);              
         }
 
         setPosition (x,y) {
@@ -25,14 +24,23 @@
           this.enemyType = type;
           switch (this.enemyType) {
           case 1: // klasyczny pocisk
-            this.w=50;
-            this.h=50;
-            this.img.src = "img/Asteroid.png";          
+          this.enable=true;
+          this.w=50;
+          this.h=50;
+          this.img.src = "img/Asteroid.png";          
+          this.life=1;
+          this.speed =3;
+          this.how=15
             break;
           case 2: // zmodyfikowany pocisk
-            this.w=100;
-            this.h=100;
-            this.img.src = "img/Asteroid.png";          
+            this.enable=true;
+            this.how=15;
+            this.cout= Math.floor((Math.random() * 100) + 1);
+            this.w=140;
+            this.h=140;
+            this.img.src = "img/Asteroid_big.png";     
+            this.life=10;     
+            this.speed=2;
             break;
           }
         }
@@ -40,8 +48,14 @@
         speedUp() {
           this.y += this.speed;
         }
+        randomCout(){
+          this.cout= Math.floor((Math.random() * 100) + 1);
+        }
         decreaseLife(){
           this.life-=1;
+        }
+        decraseCout(){
+          this.cout-=1;
         }
       }
       
--- main.js
+++ main.js
@@ -18,30 +18,33 @@ canvas.addEventListener("mousemove", playerPosition);
 let gameStart = false;
 let instervalGame;
 let score = 0;
-let bigCout = Math.floor((Math.random() * 21) + 1);
-console.log(bigCout);
+let mode = "easy";
+let bigCout = Math.floor((Math.random() * 100) + 1);
+let gameidx = 0;
 let bullets = [];
 bullets.length = 5;
 let enemy = [];
 enemy.length = 5;
-for (let i = 0; i < bullets.length; i++) {
-  bullets[i] = new Bullet(0, ch - 80, 30, 30, 10);
+
+function modes() {
+  for (let i = 0; i < bullets.length; i++) {
+    bullets[i] = new Bullet(0, ch - 80, 30, 30, 10);
+  }
+  for (let i = 0; i < enemy.length; i++) {
+    enemy[i] = new enemyClass();
+    enemy[i].setType(1);
+  }
+  //big guy
+  enemy[enemy.length - 1].setType(2);
+  //fast guy
 }
-for (let i = 0; i < enemy.length; i++) {
-  enemy[i] = new enemyClass();
-}
-//big guy
-enemy[enemy.length-1].setType(2);
-enemy[enemy.length-1].setLife(10);
-enemy[enemy.length-1].setSpeed(1);
-// for (let i = 0; i < enemyBig.length; i++) {
-//   enemyBig[i] = new enemyClass(0, 20,100,100, 2,10);
-// }
-let gameidx = 0;
 /*
 @makotek keydown eventListener na document i w callbacku event.key === "Enter"
 */
 
+canvas.onmouseover = () => {
+  document.getElementById("canvas").style.cursor = "crosshair";
+}
 
 function startScreen() {
   console.log("startScreen");
@@ -64,35 +67,107 @@ function startScreen() {
   ctx.fillStyle = "red";
   ctx.fillRect(cw / 2 - 20, ch - 60, gunClass.w, gunClass.h);
   ctx.font = "50px Helvetica";
-  ctx.fillStyle = "white";
+  ctx.fillStyle = "green";
   ctx.textAlign = "center";
-  ctx.fillText("Hello", cw / 2, ch / 2);
+  ctx.fillText("What's up?", cw / 2, ch / 2);
+  ctx.fillStyle = "white";
   ctx.font = "25px Helvetica";
-  ctx.fillText("click enter to begin", cw / 2, ch / 2 + 30);
+  ctx.fillText("select mode to begin", cw / 2, ch / 2 + 30);
   scoreFunction()
-
-  $(document).keypress(function (e) {
-    if (e.which == 13 || event.keyCode == 13) {
-      console.log("KEY PRESSED");
-      gameStart = true;
-      canvas.onclick = () => {
-        if (!gameStart) return;
-        for (let i = 0; i < bullets.length; i++) {
-          if (bullets[i].visible == false) {
-            bullets[i].visible = true;
-            bullets[i].x = gunClass.playerX + (gunClass.w - bullets[i].w) / 2;
-            break;
-          }
-        }
+  document.getElementById("easy").onclick =()=>{
+    setMode("easy")
+  }
+  document.getElementById("medium").onclick =()=>{
+    setMode("medium")
+  }
+  document.getElementById("hard").onclick =()=>{
+    setMode("hard")
+  }
+  canvas.onclick = () => {
+    if (!gameStart) return;
+    for (let i = 0; i < bullets.length; i++) {
+      if (bullets[i].visible == false) {
+        bullets[i].visible = true;
+        bullets[i].x = gunClass.playerX + (gunClass.w - bullets[i].w) / 2;
+        break;
       }
-      gunClass.visible = true;
     }
-  });
+  }
+  gunClass.visible = true;
+
 }
+function setMode(modee){
+  gunClass.visible = true;
+  gameStart = true;
+  mode=modee;
+  for(let e=0;e<3;++e){
+  document.getElementsByClassName('button')[e].style.display = 'none';
+  }
+}
+
+
 window.onload = () => {
+  modes()
   startScreen()
   instervalGame = setInterval(game, 1000 / 60);
 }
+
+function overScreen() {
+  for(let e=0;e<3;++e){
+    document.getElementsByClassName('button')[e].style.display = 'block';
+    }
+  gameStart = false;
+  /*start screen*/
+  for (let i = 0; i < enemy.length; i++) {
+    console.log("speed=" + enemy[i].speed);
+    enemy[i].visible = false;
+    if(enemy[i].type==2){
+    enemy[i].randomCout();
+    }
+  }
+  for (let i = 0; i < bullets.length; i++) {
+    bullets[i].visible = false;
+  }
+
+  gunClass.visible = false;
+  ctx.clearRect(0, 0, cw, ch);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cw, ch);
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, ch - 20, cw, ch);
+  ctx.fillStyle = "red";
+  ctx.fillRect(cw / 2 - 20, ch - 60, gunClass.w, gunClass.h);
+  ctx.font = "50px Helvetica";
+  ctx.fillStyle = "red";
+  ctx.textAlign = "center";
+  ctx.fillText("GAME OVER", cw / 2, ch / 2);
+  ctx.font = "25px Helvetica";
+  ctx.fillStyle = "white";
+  ctx.fillText("select mode to play again", cw / 2, ch / 2 + 30);
+  scoreFunction()
+
+  scoreFunction()
+  document.getElementById("easy").onclick =()=>{
+    setMode("easy")
+  }
+  document.getElementById("medium").onclick =()=>{
+    setMode("medium")
+  }
+  document.getElementById("hard").onclick =()=>{
+    setMode("hard")
+  }
+  canvas.onclick = () => {
+    if (!gameStart) return;
+    for (let i = 0; i < bullets.length; i++) {
+      if (bullets[i].visible == false) {
+        bullets[i].visible = true;
+        bullets[i].x = gunClass.playerX + (gunClass.w - bullets[i].w) / 2;
+        break;
+      }
+    }
+  }
+}
+
 
 
 
@@ -128,7 +203,7 @@ function strzal() {
     }
     if (bullets[i].visible == true) {
       ctx.drawImage(bullets[i].img, bullets[i].x, bullets[i].y, bullets[i].w, bullets[i].h);
-      for (let x = 0; x < enemy.length; x++) {        
+      for (let x = 0; x < enemy.length; x++) {
         if (!enemy[x].visible) continue;
         //else if ((enemy[x].y >= bullets[i].y) && ((enemy[x].x >= bullets[i].x - (enemy[x].x+bullets[i].x)/2) && (enemy[x].x <= bullets[i].x + (enemy[x].x+bullets[i].x)/2))) {
         if ((enemy[x].y + enemy[x].h >= bullets[i].y) &&
@@ -136,24 +211,24 @@ function strzal() {
             ((bullets[i].x >= enemy[x].x) && (bullets[i].x <= enemy[x].x + enemy[x].w)) ||
             ((bullets[i].x + bullets[i].w <= enemy[x].x + enemy[x].w) && (bullets[i].x + bullets[i].w >= enemy[x].x))
           )) {
-            if(enemy[x].enemyType==1){
+          if (enemy[x].enemyType == 1) {
+            ++score;
+            bullets[i].visible = false;
+            enemy[x].visible = false;
+            bullets[i].y = ch - 80;
+          } else if (enemy[x].enemyType == 2) {
+            if (enemy[x].life <= 0) {
               ++score;
               bullets[i].visible = false;
               enemy[x].visible = false;
+              enemy[x].setLife(10);
               bullets[i].y = ch - 80;
-            }else if(enemy[x].enemyType==2){
-              if(enemy[x].life<=0){
-                ++score;
-                enemy[x].setLife(10);
-                bullets[i].visible = false;
-                enemy[x].visible = false;
-                bullets[i].y = ch - 80;
-              }else{
-                enemy[x].decreaseLife();
-                bullets[i].visible = false;
-                bullets[i].y = ch - 80;
-              }
+            } else {
+              enemy[x].decreaseLife();
+              bullets[i].visible = false;
+              bullets[i].y = ch - 80;
             }
+          }
         }
       }
       bullets[i].speedUp();
@@ -165,9 +240,9 @@ function meteoAnimate() {
   for (let i = 0; i < enemy.length; i++) {
     if (enemy[i].visible == true) {
       ctx.drawImage(enemy[i].img, enemy[i].x, enemy[i].y, enemy[i].w, enemy[i].h);
-      if (enemy[i].y+enemy[i].h >= ch - 20) {
+      if (enemy[i].y + enemy[i].h >= ch - 20) {
         score = 0;
-        startScreen();
+        overScreen();
       } else {
         console.log("speedUp");
         enemy[i].speedUp();
@@ -179,43 +254,51 @@ function meteoAnimate() {
 function scoreFunction() {
   ctx.fillStyle = "white";
   ctx.font = "30px Helvetica";
-  ctx.fillText(score, cw - 40, 0 + 80);
+  ctx.fillText(score, cw - 40, 60);
+  ctx.font = "25px Helvetica";
+  ctx.fillText(mode, cw - 50, 90);
 }
 
 function game() {
   if (!gameStart) return;
 
-  gameidx++;
-  if ((gameidx % 15) == 0) {
-    console.log("cout=" + bigCout);
-    bigCout--;
-    if (bigCout <= 0) {
-      bigCout = Math.floor((Math.random() * 100) + 1);
-      for (let i = 0; i < enemy.length; i++) {
-        if ((enemy[i].visible == false) && (enemy[i].enemyType == 2)) {
-          console.log("GENERUJE");
-          enemy[i].setPosition(Math.floor(Math.random() * (450 - 50)), 20);
-          enemy[i].visible = true;
+  ++gameidx;
+  for (let x = 0; x < enemy.length; x++) {
+    if ((gameidx % enemy[x].how) == 0) {
+      if (enemy[x].cout <= 0) {
+        for (let i = 0; i < enemy.length; i++) {
+          if ((enemy[i].visible == false) && (enemy[i].enemyType == 2)) {
+            console.log("GENERUJE");
+            enemy[i].setPosition(Math.floor(Math.random() * (450 - 50)), 20);
+            enemy[i].visible = true;
+            enemy[i].randomCout();
+          }
+        }
+      } else {
+        for (let i = 0; i < enemy.length; i++) {
+          if ((enemy[i].visible == false) && (enemy[i].enemyType == 1)) {
+            enemy[i].setPosition(Math.floor(Math.random() * (450 - 50)), 20);
+            enemy[i].visible = true;
+            console.log(enemy[i].x);
+            continue;
+          } else if ((enemy[i].visible == false) && (enemy[i].enemyType == 2)) {
+            enemy[x].decraseCout();
+            console.log("cout=" + enemy[i].cout);
+            continue;
+          }
         }
       }
     }
-    for (let i = 0; i < enemy.length; i++) {
-      if ((enemy[i].visible == false) && (enemy[i].enemyType == 1)) {
-        enemy[i].setPosition(Math.floor(Math.random() * (450 - 50)), 20);
-        enemy[i].visible = true;
-        console.log(enemy[i].x);
-        break;
-      }
-    }
+
   }
 
-  
+
   table()
   ground()
   strzal()
   scoreFunction()
-  
+
   meteoAnimate()
   gun()
-  
+
 }
