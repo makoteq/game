c6df51b244662c4693902e27327c3886c2fc084f
Maciej Kotowski <makoteq@gmail.com>
2020-04-05 18:44:06 +0200

    add history

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let worker = {
 };
 let builder = {
   count: 0,
-  prize: 2,
+  prize: 36,
 };
 let clicker = {
   count: 0,
@@ -77,7 +77,12 @@ let data = [
   {
     name: "Katedra Św. Michała Archanioła",
     img: "./img/katedra/K",
-    description: "",
+    description: "Zgodę na budowę kościoła uzyskano po długich staraniach w 1886, kiedy liczba wiernych w praskiej parafii przy kościele Najświętszej Matki Bożej Loretańskiej sięgnęła ok. 32 tys. \
+Z uwagi na ubóstwo mieszkańców, car Aleksander III wyraził zgodę na zbiórkę pieniędzy także poza Warszawą.\
+ Przed zatwierdzeniem dokumentacji dokonano zmian w projekcie, wymuszonych względami oszczędnościowymi. W dniu wmurowania kamienia węgielnego zebrane fundusze wynosiły ok. 50 tys. rubli, z czego 6 tys.\
+ rubli podarowała Aleksandra Potocka. Z uwagi na podmokłość terenu, zlokalizowanego na nadwiślańskich błoniach, konieczne było zastosowanie wzmocnionych fundamentów. Do budowy kościoła zamówiono cegły o podwyższonej wytrzymałości mechanicznej wytwarzane ręcznie w zakładach Kazimierza Granzowa. \
+Kościół został konsekrowany 29 września 1901 – w dniu patronalnym św. Michała Archanioła – przez bpa Kazimierza Ruszkiewicza. W uroczystości uczestniczyło 30 tys. osób. Świątynia mogła pomieścić ok. 8000 wiernych. Koszt jej budowy wyniósł 300 tys. rubli. Kościół został wysadzony w powietrze przez wycofujących się Niemców 14 września 1944.\
+ Do wysadzenia świątyni użyto także materiałów wybuchowych przeznaczonych pierwotnie do zniszczenia sąsiedniego Szpitala Praskiego, w wyniku czego szpital ocalał. Z całej budowli ocalały fragmenty ścian bocznych z figurami patronów świątyni, św. Michała Archanioła i św. Floriana Męczennika, wykonanymi przez Tadeusza Skoniecznego.   ",
     legendary: "1",
     width: "264",
     height: "384",
@@ -86,7 +91,8 @@ let data = [
   {
     name: "Pałac Jana 3 Sobieskiego",
     img: "./img/PalacJ3S/PK",
-    description: "",
+    description: "Barokowy pałac królewski znajdujący się w Warszawie, w dzielnicy Wilanów. Został wzniesiony w latach 1681–1696 dla króla Jana III Sobieskiego i Marii Kazimiery według projektu Augustyna Wincentego Locciego; skrzydła boczne dobudowano w latach 1723–1729.Podczas II wojny światowej Niemcy i Węgrzy zagrabili ok. 80% wyposażenia wnętrz pałacu, zniszczono także ogród pałacowy.\
+ Przejęty po ostatniej wojnie na własność państwa, po gruntownych pracach konserwatorskich i rewaloryzacyjnych oraz rewindykacji znacznej części zbiorów wywiezionych przez Niemców, został udostępniony publiczności w 1962 roku. ",
     legendary: "1",
     width: "380",
     height: "142",
@@ -95,16 +101,22 @@ let data = [
   {
     name: "Pałac kultury",
     img: "./img/PalacKult/PK",
-    description: "",
+    description: "Pałac Kultury i Nauki- Najwyższy budynek w Polsce, razem ze wspornikiem antenowym, będącym integralną częścią iglicy, ma wysokość 237 metrów. Znajdujący się w śródmieściu Warszawy na placu Defilad 1.\
+ Od 2007 gmach znajduje się w rejestrze zabytków. Pałac stanowił „dar narodu radzieckiego dla narodu polskiego”. Wybudowany został w latach 1952–1955 według projektu radzieckiego architekta Lwa Rudniewa, który inspirował się moskiewskimi drapaczami chmur.    ",
     legendary: "1",
     width: "392",
     height: "504",
     frames: "63",
   },
   {
-    name: "Pałac Królewski",
+    name: "Zamek Królewski",
     img: "./img/PalacKr/PK",
-    description: "",
+    description: "Pierwotnie rezydencja książąt mazowieckich, a od XVI wieku siedziba władz I Rzeczypospolitej: króla i Sejmu.\
+W swojej długiej historii Zamek Królewski był wielokrotnie grabiony i dewastowany przez wojska szwedzkie, brandenburskie, niemieckie i rosyjskie.\
+ W XIX wieku, po upadku powstania listopadowego, przeznaczony na potrzeby administracji rosyjskiej. W okresie I wojny światowej rezydencja niemieckiego generalnego gubernatora.\
+ W latach 1920–1922 siedziba Naczelnika Państwa, w latach 1926–1939 rezydencja Prezydenta Rzeczypospolitej Polskiej. Spalony i ograbiony przez Niemców w 1939, niemal doszczętnie zniszczony w 1944.\
+W 1980 Zamek Królewski wraz ze Starym Miastem został wpisany na listę światowego dziedzictwa UNESCO.\
+   ",
     legendary: "1",
     width: "368",
     height: "260",
@@ -113,7 +125,12 @@ let data = [
   {
     name: "Pałac Łazienkowski",
     img: "./img/PalacLaz/P",
-    description: "",
+    description: "Początki dzisiejszego Pałacu na Wyspie sięgają końca XVII w. \
+ Na polecenie księcia Stanisława Herakliusza Lubomirskiego, jednego z największych polityków, pisarzy i filozofów tych czasów, wybudowana została Łaźnia. \
+ Barokowy pawilon ogrodowy, projektu holenderskiego architekta Tylmana z Gameren, miał służyć zarówno odpoczynkowi, rozrywce, jak i kontemplacji. Wnętrze Łaźni skrywało salę stylizowaną na grotę. Biło w niej źródło, symbolizujące źródło w starożytnej Grecji, niosące muzom natchnienie.W 1764 r.\
+ Stanisław August, szukając miejsca na swą letnią królewską rezydencję, zakupił Łaźnię wraz z Ujazdowem. Za sprawą architektów - pochodzącego z Włoch Domenico Merliniego i urodzonego w Dreźnie Jana Chrystiana Kamsetzera - król przekształcił barokowy pawilon Łaźni w klasycystyczny Pałac na Wyspie. \
+Stanisław August przekształcił Pałac na Wyspie w willę muzeum, w której eksponowane były najcenniejsze obrazy z jego kolekcji, liczącej według inwentarza z 1795 r. 2289 dzieł najważniejszych artystów i szkół europejskich XVII i XVIII w. Najliczniej reprezentowanymi malarzami byli artyści holenderscy,\
+ z których dzieł do najcenniejszych zaliczyć należy obrazy autorstwa Rembrandta van Rijn.",
     legendary: "1",
     width: "320",
     height: "132",
@@ -142,7 +159,7 @@ function showPage() {
 }
 let loader = {
   loadedCount: 0,
-  totalCount: 123,
+  totalCount: 408,
   loadImage: (url) => {
     loader.loadedCount++;
     let img = new Image(); // Create new img element
@@ -202,11 +219,11 @@ function click() {
       history.push(data[instance]);
       history.forEach((element) => {
         document.getElementById("history").innerHTML =
-          document.getElementById("history").innerHTML +
-          '<img src="' +
-          element.img +
-          element.frames +
-          '.png" width=100 style="cursor:pointer;margin:8px" alt="">';
+        document.getElementById("history").innerHTML +
+        '<img src="' +
+        element.img +
+        element.frames +
+        '.png" data-toggle="modal" onclick="showdata(`'+element.name+'`,`'+element.description+'`)"   data-target="#description" width=100  style="cursor:pointer;margin:8px" alt="">'
       });
     }
     frame = 1;
@@ -220,6 +237,10 @@ function click() {
     }
   }
   loader.loadImage(data[instance].img + frame + ".png");
+}
+function showdata(name,des){
+  document.getElementById("descriptionTitle").innerText = name;
+  document.getElementById("descriptionDes").innerText = des;
 }
 function update(arg) {
   switch (arg) {
